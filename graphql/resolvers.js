832a8f200cd7bbc1998512e552b88d5ad4764eda
taeyoung <xoxodudwkd@gmail.com>
2020-05-18 22:22:15 +0900

    Creating the first GraphQL and Resolver

---
+++ graphql/resolvers.js
@@ -0,0 +1,7 @@
+const resolvers = {
+  Query: {
+    name: () => 'youngs',
+  },
+};
+
+export default resolvers;
--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 import { GraphQLServer } from 'graphql-yoga';
+import resolvers from './graphql/resolvers';
 
-console.log('blah');
+const server = new GraphQLServer({
+  typeDefs: 'graphql/schema.graphql',
+  resolvers,
+});
+
+server.start(() => console.log('GraphQL Server Running'));
